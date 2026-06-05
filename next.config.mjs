f3572a88c8ce21f15d3a77416d5b6f9c8948a063
Panb-KG <paulpan99@163.com>
2026-06-05 16:26:20 +0800

    fix: 增加请求体大小限制为20MB

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    bodyParser: {
+      sizeLimit: '20mb',
+    },
+  },
   images: {
     remotePatterns: [
       {
